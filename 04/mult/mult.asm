bb44c8a5a12bb09b707c6630444a1e699031f43c
Yaguchi <tomoya0229@gmail.com>
2019-02-08 00:13:27 +0900

    ch04 multiply comp

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,55 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @0
+    D=A
+    @R2
+    M=D
+    @R0
+    D=M
+    @R1
+    D=D-M
+    @LARGERR0
+    D;JGE
+    @LARGERR1
+    0;JMP
+(LARGERR0)
+    @R1
+    D=M
+    @num
+    M=D
+    @R0
+    D=M
+    @addnum
+    M=D
+    @LOOP
+    0;JMP
+(LARGERR1)
+    @R0
+    D=M
+    @num
+    M=D
+    @R1
+    D=M
+    @addnum
+    M=D
+    @LOOP
+    0;JMP
+(LOOP)
+    @1   //A = 1
+    D=A  // D = 1
+    @num   
+    M=M-D // num = num - 1
+    D=M
+    @END
+    D;JLT
+    @addnum
+    D=M // D=addnum
+    @R2
+    M=M+D // R2 = R2 + addnum
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
